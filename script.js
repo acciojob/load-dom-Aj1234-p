7f6a2791b1fd5ea75750151de02b08970531c80d
Ajit Pandey <ajitytsurat0707@gmail.com>
2026-07-29 11:00:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-//your JS code here. If required.
-let body = document.querySelector('body');
+//your JS code here. If require
+let body = document.querySelector('body'); 
 window.addEventListener('DOMContentLoaded',(e)=>{
-        console.log("DOM load success");	
+        body.textContent = "DOM load success";	
 })
